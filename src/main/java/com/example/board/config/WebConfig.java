2200524798d1a5e08a2336af3310eab349648be4
inkyu <inkyu0034@gmai.com>
2024-05-23 14:43:59 +0900

    이미지 불러오기 화면 구현 (이미지가 깨져서 나옴... 저장은 잘 되는데)

---
+++ src/main/java/com/example/board/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.board.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private String resourcePath = "/upload/**"; // viwe에서 사용할 경로
+    private String savePath = "file:///C:/ProjectHwang/spring_upload_files/";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(resourcePath)
+                .addResourceLocations(savePath);
+    }
+}
